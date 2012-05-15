46a22ec5e31a058f8792d2bc5d5bee1f83d24abc
Alexander Simonov <alex_r_10@abv.bg>
2012-05-15 21:42:47 +0300

    added money and some arrays

---
+++ Poker/src/Game/BoardTest.java
@@ -0,0 +1,26 @@
+package Game;
+
+import static org.junit.Assert.*;
+
+import java.awt.Image;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+
+import org.junit.Test;
+
+
+
+public class BoardTest {	
+	
+	@Test
+	public void PlayerCardsTest() {
+		Board board = new Board();
+		board.RefreshDeck();
+		board.Player1Cards();
+      //  assertEquals(p1card1, aa.getImage());		
+	}
+	
+}
+
